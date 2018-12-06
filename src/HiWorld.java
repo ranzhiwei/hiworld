c24883132ad4bedbe7fda70587e8df929b54478a
ranzhiwei <15175657842@163.com>
2018-12-06 16:09:30 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("Hi World! test");
-        System.out.println("Hello world");
+        System.out.println("Hello world test");
     }
 }
